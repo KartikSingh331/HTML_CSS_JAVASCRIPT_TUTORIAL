14ea591bca19e3ae8f271593cc1ca26c7976674d
KartikSingh331 <kartiksinh551@gmail.com>
2025-11-14 19:28:36 +0530

    Add files via upload

---
+++ CWH/Javascript/1/57_loops/scr.js
@@ -0,0 +1,25 @@
+let age=98;
+let grace=2;
+
+console.log(age+grace)
+console.log(age-grace)
+console.log(age*grace)
+console.log(age/grace)
+console.log(age**grace)
+console.log(age%grace)
+
+if ((age+grace)>43){
+    console.log("You are old");
+}
+else{
+    console.log("you are young")
+}
+
+// age+=grace;   assignment operator
+
+//ternary operator
+a=6
+b=8
+c=a>b ?(a-b):(b-a)  
+console.log(c)
+// ager a>b ho to return a-b else b-a 
