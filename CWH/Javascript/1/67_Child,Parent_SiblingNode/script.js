68f66e9e54137228be3084201b7f0d3d16f3152b
KartikSingh331 <kartiksinh551@gmail.com>
2025-11-17 22:09:12 +0530

    Add files via upload

---
+++ CWH/Javascript/1/67_Child,Parent_SiblingNode/script.js
@@ -0,0 +1,7 @@
+console.log(document.body.childNodes);
+console.log(document.body.childNodes[1]);
+let cont=document.body.childNodes[1]
+console.log(cont.firstElementChild);// same last child
+
+cont.lastElementChild.style.color="red"
+document.firstElementChild
