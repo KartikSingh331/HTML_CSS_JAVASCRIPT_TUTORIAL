8ccf1345c074aa1f296bbbbd4c848012503ad429
KartikSingh331 <kartiksinh551@gmail.com>
2025-11-22 10:21:18 +0530

    Add files via upload

---
+++ CWH/Javascript/1/75_jsCallback&Premises/script.js
@@ -0,0 +1,14 @@
+let button = document.getElementById("btn")
+
+button.addEventListener("click", ()=>{
+    // alert("I was hovered madherchod....Ma ka bhosda aaag....bhag bhosdi aaag")
+    // document.querySelector(".box").innerHTML= "<b> jsjsjjjdcjdjcjcjdcccjcjcj<b>"
+})
+
+// button.addEventListener("context menu", ()=>{
+// alert("Dont hack me by right click")
+// })
+
+button.addEventListener("keydown", (e)=>{
+   console.log(e)
+})
