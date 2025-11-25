7aeb6b1a393ea7360f397aa8e319dabfaff449d1
KartikSingh331 <kartiksinh551@gmail.com>
2025-11-25 22:40:00 +0530

    Add files via upload

---
+++ CWH/Javascript/1/80_Class&Object/script.js
@@ -0,0 +1,14 @@
+ let obj ={
+    a: 1,
+    b: "karry"
+ }
+ console.log(obj)
+
+ let animal = {
+    eats: True
+ };
+ let rabbit = {
+    jump: True
+ };
+
+ rabbit._proto_ = animal; // sets rabbit.{{PrtoType}} = animal
