d184b638f43cc419a32ede1491e9972ab37a5cc9
KartikSingh331 <kartiksinh551@gmail.com>
2025-11-28 19:42:45 +0530

    Add files via upload

---
+++ CWH/Javascript/1/60_String_2.js
@@ -0,0 +1,23 @@
+console.log("This is string tutorial")
+let a="Hello"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])   // no errors shoes undefined shows
+
+console.log(a.length)   //string is imutable(not change)
+
+let rname = "Kartik";
+let friend = "Karan";
+
+console.log(`His name is ${rname} and his friend name is ${friend}`);
+
+console.log(a.toUpperCase())
+console.log(a.toLowerCase())
+console.log(rname.slice(1,5))
+console.log(rname.slice(1))
+
+console.log(a.replace("ll","mm"))
+console.log(a.concat(rname),"kaho")
